c871e2c0bb76f6f67169642c7d9a5b839742a157
Aslak Johannessen <aslakjo@gmail.com>
2011-04-29 17:26:52 +0200

    Adding the lære flow, which takes you trhoug the questions.

--- src/main/scala/com/chengin/web/HelloWorld.scala
+++ src/main/scala/com/chengin/web/HelloWorld.scala
@@ -5,12 +5,11 @@ import scalate.ScalateSupport
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.adapters.H2Adapter
 import org.squeryl.{Session, SessionFactory}
-import no.aslakjo.bekkquiz.model.{Question, QuizModel}
+import no.aslakjo.bekkquiz.model.{QuizModel, QuestionSet, Question}
+import org.scalatra.ScalatraKernel._
 import QuizModel._
 
-class HelloWorld extends ScalatraServlet with ScalateSupport {
-
-
+class HelloWorld extends ScalatraServlet with ScalateSupport with Gruppe with Sporsmal with Laere {
   before {
     contentType = "text/html"
     SessionFactory.newSession.bindToCurrentThread
@@ -42,9 +41,62 @@ class HelloWorld extends ScalatraServlet with ScalateSupport {
   get("/"){
     <p>
       <h1>Dette kan du gjøre</h1>
+      <a href="/laere">Lære</a>
+
       <a href="/sporsmal">Spørsmål</a>
+      <a href="/gruppe">Spørsmål grupper</a>
     </p>
   }
+
+  protected def contextPath = request.getContextPath
+}
+
+trait Laere extends ScalatraServlet with ScalateSupport{
+  def currentSet(id:Int) = from(questionSet)(s => where(s.id === id) select(s)).single
+
+
+  get("/laere"){
+    val allSets = from(questionSet)(s=> select(s)).toList
+
+    templateEngine.layout("/WEB-INF/laere.scaml",
+      Map("sporsmalSet" -> allSets)
+    )
+  }
+
+  get("/laere/gjennom/:id"){
+    redirect("/laere/gjennom/%s/sp/0".format(params("id")))
+  }
+
+  get("/laere/gjennom/:id/sp/:spId"){
+    val set  = currentSet(params("id").toInt)
+    val spId = params("spId").toInt
+    val question = set.questions.toList.apply(spId)
+
+    templateEngine.layout("/WEB-INF/svare.scaml",
+      Map("sporsmal" -> question)
+    )
+  }
+
+  post("/laere/gjennom/:id/sp/:spId"){
+    val set = currentSet(params("id").toInt)
+    val questionId = params("spId").toInt
+    val nextQuestionId = questionId + 1
+
+    if(nextQuestionId >= set.questions.size){
+      redirect("/laere/gjennom/%s/ferdig".format(params("id")))
+    }else{
+      val question = set.questions.toList.apply(questionId)
+      redirect("/laere/gjennom/%s/sp/%s".format(params("id").toInt, params("spId").toInt +1))
+    }
+  }
+
+  get("/laere/gjennom/:id/ferdig"){
+    <h1>Ferdig!</h1>
+  }
+}
+
+trait Sporsmal extends ScalatraServlet with ScalateSupport {
+  def questionById(id:Int) = from(questions)(s => where(s.id == id) select(s)).single
 
   get("/sporsmal") {
     val allQuestions: List[Question] = from(questions)(s => select(s)).toList
@@ -102,8 +154,41 @@ class HelloWorld extends ScalatraServlet with ScalateSupport {
 
     redirect("/sporsmal")
   }
+}
 
-  def questionById(id:Int) = from(questions)(s => where(s.id == id) select(s)).single
+trait Gruppe extends ScalatraServlet with ScalateSupport  {
+  get("/gruppe"){
+    val allGroups: List[QuestionSet] = from(questionSet)(s => select(s)).toList
 
-  protected def contextPath = request.getContextPath
+    templateEngine.layout("/WEB-INF/set.scaml",
+      Map("sporsmalSet" -> allGroups)
+    )
+  }
+
+  get("/gruppe/nytt"){
+    val allQuestions = from(questions)(q => select(q)).toList
+
+    templateEngine.layout("/WEB-INF/edit-set.scaml",
+      Map("questions" -> allQuestions)
+    )
+  }
+
+  post("/gruppe/nytt"){
+    val questionIds = multiParams("questions").map(_.toLong)
+    val questionGroup = new QuestionSet(params("name"), params("desc"))
+    questionSet.insert(questionGroup)
+
+    questionIds.foreach(id =>{
+      val question = from(questions)(q=> where(q.id === id) select(q)).single
+      questionGroup.questions.associate(question)
+    })
+
+    redirect("/gruppe")
+  }
+
+  get("/gruppe/slett/:id"){
+    questionSet.deleteWhere(qs => qs.id === params("id").toInt)
+
+    redirect("/gruppe")
+  }
 }
--- src/main/scala/no/aslakjo/bekkquiz/model/Model.scala
+++ src/main/scala/no/aslakjo/bekkquiz/model/Model.scala
@@ -2,6 +2,7 @@
 package no.aslakjo.bekkquiz.model;
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.{KeyedEntity, Schema}
+import org.squeryl.dsl.CompositeKey2
 
 class Question (
   val alternativ1: String,
@@ -19,12 +20,38 @@ class Question (
     title, question,
     alternativ1, alternativ2, alternativ3, alternativ4, alternativ5
   )
+
+  lazy val sets = QuizModel.questionsAssosiatedSets.left(this)
+}
+
+class SetComposedOfQuestion(
+  val questionId : Long,
+  val setId : Long
+) extends KeyedEntity[CompositeKey2[Long,Long]] {
+  def id = compositeKey(questionId, setId)
+}
+
+class QuestionSet(
+  val name :String,
+  val description :String
+) extends KeyedEntity[Long]{
+  val id :Long = 0
+  def this() = this("","")
+
+  lazy val questions = QuizModel.questionsAssosiatedSets.right(this)
 }
 
 
 object QuizModel extends Schema {
 
   val questions = table[Question]("QUESTIONS")
+  val questionSet = table[QuestionSet]
+
+  val questionsAssosiatedSets =
+    manyToManyRelation(questions, questionSet)
+      .via[SetComposedOfQuestion](
+        (q,s, composed) => (q.id === composed.questionId, s.id === composed.setId)
+      )
 
   on(questions)(q => declare(
       q.question    is(dbType("varchar(512)")),
